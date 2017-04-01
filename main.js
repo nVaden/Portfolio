09ff981ce11e73531a3aa5611afbf13d8af5a1b0
Nicholas Vaden <nickvaden11@Live.com>
2017-04-01 19:51:39 +0200

    First attempt quick email html

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// Okay lets do this
+function sendMail() {
+    var link = "mailto:me@example.com"
+             + "?cc=myCCaddress@example.com"
+             + "&subject=" + escape("This is my subject")
+             + "&body=" + escape(document.getElementById('myText').value)
+    ;
+
+    window.location.href = link;
+}
