b89b5c3650897a754898daf160e32398b5350d47
Nicholas Vaden <vaden.nicholas@gmail.com>
2017-09-25 17:28:21 +0800

    touch

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function($){
+
+
+	var spinArray = ['animation900','animation1080','animation1260','animation1440','animation1620','animation1800','animation1980','animation2160'];
+
+	function getSpin() {
+		var spin = spinArray[Math.floor(Math.random()*spinArray.length)];
+		return spin;
+	}
+
+	$('#coin').on('click', function(){
+
+		$('#coin').removeClass();
+
+		setTimeout(function(){
+			$('#coin').addClass(getSpin());
+		}, 100);
+
+	});
 
 
 	$("a").on('click', function(event){
@@ -22,3 +40,5 @@ $(document).ready(function(){
 		}
 	});
 });
+
+
